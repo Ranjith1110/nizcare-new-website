4857be22a4c27d038c3eaf9a1020d77e9daa8d60
Ranjith <ranjithram878@gmail.com>
2024-09-26 12:22:25 +0530

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-document.getElementById("menuBar").addEventListener("click", function() {
-    document.getElementById("slider").classList.add("show"); // Show the sidebar by adding the 'show' class
-    document.getElementById("menuBar").style.display = "none"; // Hide the menu bar icon
-    document.getElementById("closeIcon").style.display = "inline"; // Show the close icon
+document.getElementById("menuBar").addEventListener("click", function () {
+    document.getElementById("slider").classList.add("show");
+    document.getElementById("menuBar").style.display = "none";
+    document.getElementById("closeIcon").style.display = "inline";
 });
 
-document.getElementById("closeIcon").addEventListener("click", function() {
-    document.getElementById("slider").classList.remove("show"); // Hide the sidebar by removing the 'show' class
-    document.getElementById("menuBar").style.display = "inline"; // Show the menu bar icon
-    document.getElementById("closeIcon").style.display = "none"; // Hide the close icon
+document.getElementById("closeIcon").addEventListener("click", function () {
+    document.getElementById("slider").classList.remove("show");
+    document.getElementById("menuBar").style.display = "inline";
+    document.getElementById("closeIcon").style.display = "none";
 });
